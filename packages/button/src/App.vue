8ac0dd600e9275284515f2220ab70b0692b8ccad
ddzy <1766083035@qq.com>
2021-04-13 14:35:27 +0800

    feat(button): 更新 Button 组件, 新增禁用按钮配置项
    affects: button

--- packages/button/src/App.vue
+++ packages/button/src/App.vue
@@ -1,47 +1,99 @@
 <template>
-	<div class="button__list">
-		<V3Button
-			class="button__item"
-			type="default"
-			@click="handleClick($event, 'default')"
-		>
-			默认按钮
-		</V3Button>
-		<V3Button
-			class="button__item"
-			type="primary"
-			@click="handleClick($event, 'primary')"
-		>
-			主按钮
-		</V3Button>
-		<V3Button
-			class="button__item"
-			type="success"
-			@click="handleClick($event, 'success')"
-		>
-			成功按钮
-		</V3Button>
-		<V3Button
-			class="button__item"
-			type="danger"
-			@click="handleClick($event, 'danger')"
-		>
-			危险按钮
-		</V3Button>
-		<V3Button
-			class="button__item"
-			type="warning"
-			@click="handleClick($event, 'warning')"
-		>
-			警告按钮
-		</V3Button>
+	<div class="button-container">
+		<div class="button__list">
+			<V3Button
+				class="button__item"
+				type="default"
+				@click="handleClick($event, 'default')"
+			>
+				默认按钮
+			</V3Button>
+			<V3Button
+				class="button__item"
+				type="primary"
+				@click="handleClick($event, 'primary')"
+			>
+				主按钮
+			</V3Button>
+			<V3Button
+				class="button__item"
+				type="success"
+				@click="handleClick($event, 'success')"
+			>
+				成功按钮
+			</V3Button>
+			<V3Button
+				class="button__item"
+				type="danger"
+				@click="handleClick($event, 'danger')"
+			>
+				危险按钮
+			</V3Button>
+			<V3Button
+				class="button__item"
+				type="warning"
+				@click="handleClick($event, 'warning')"
+			>
+				警告按钮
+			</V3Button>
 
-		<V3Button class="button__item" borderType="dashed">
-			默认按钮 + 虚线边框
-		</V3Button>
-		<V3Button class="button__item" borderType="dotted">
-			默认按钮 + 点边框
-		</V3Button>
+			<V3Button class="button__item" borderType="dashed">
+				默认按钮 + 虚线边框
+			</V3Button>
+			<V3Button class="button__item" borderType="dotted">
+				默认按钮 + 点边框
+			</V3Button>
+		</div>
+
+		<div class="button__list">
+			<V3Button
+				:disabled="true"
+				class="button__item"
+				type="default"
+				@click="handleClick($event, 'default')"
+			>
+				默认按钮
+			</V3Button>
+			<V3Button
+				disabled
+				class="button__item"
+				type="primary"
+				@click="handleClick($event, 'primary')"
+			>
+				主按钮
+			</V3Button>
+			<V3Button
+				disabled
+				class="button__item"
+				type="success"
+				@click="handleClick($event, 'success')"
+			>
+				成功按钮
+			</V3Button>
+			<V3Button
+				disabled
+				class="button__item"
+				type="danger"
+				@click="handleClick($event, 'danger')"
+			>
+				危险按钮
+			</V3Button>
+			<V3Button
+				disabled
+				class="button__item"
+				type="warning"
+				@click="handleClick($event, 'warning')"
+			>
+				警告按钮
+			</V3Button>
+
+			<V3Button disabled class="button__item" borderType="dashed">
+				默认按钮 + 虚线边框
+			</V3Button>
+			<V3Button disabled class="button__item" borderType="dotted">
+				默认按钮 + 点边框
+			</V3Button>
+		</div>
 	</div>
 </template>
 <script lang="ts">
@@ -59,16 +111,21 @@ export default defineComponent({
 
 		return {
 			handleClick,
-		}
+		};
 	},
 });
 </script>
 <style lang="scss" scoped>
-.button__list {
-	display: flex;
-	justify-content: flex-start;
-	.button__item {
-		margin-left: 12px;
+.button-container {
+	.button__list {
+		display: flex;
+		justify-content: flex-start;
+		&:not(:first-child) {
+			margin-top: 20px;
+		}
+		.button__item {
+			margin-left: 12px;
+		}
 	}
 }
 </style>
