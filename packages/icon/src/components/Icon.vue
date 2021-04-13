1d15e6d421ae3826a17257d0f13116dd62a8c1f0
ddzy <1766083035@qq.com>
2021-04-13 18:26:10 +0800

    feat(icon): 初始化一些新的图标
    affects: icon

---
+++ packages/icon/src/components/Icon.vue
@@ -0,0 +1,292 @@
+<template>
+	<div class="icon-container">
+		<ul class="icon__list">
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-search"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-search</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-viewlarger"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-viewlarger</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-editor"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-editor</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-gift"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-gift</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-Notvisible"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-Notvisible</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-arrow-right"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-arrow-right</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-arrow-left"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-arrow-left</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-ashbin"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-ashbin</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-browse"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-browse</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-close"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-close</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-certified-supplier"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-certified-supplier</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-clock"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-clock</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-CurrencyConverter"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-CurrencyConverter</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-Customermanagement"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-Customermanagement</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-discount"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-discount</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-download"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-download</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-ellipsis"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-ellipsis</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-help"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-help</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-link"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-link</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-pin"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-pin</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-prompt"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-prompt</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-reeor"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-reeor</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-responsetime"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-responsetime</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-scanning"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-scanning</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-sound-Mute"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-sound-Mute</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-sound-filling"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-sound-filling</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-switch"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-switch</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-upload"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-upload</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-warning"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-warning</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-exchangerate"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-exchangerate</span>
+				</div>
+			</li>
+			<li class="icon__item">
+				<div class="icon-title__wrapper">
+					<i class="icon__title v3-icon v3-icon-set"></i>
+				</div>
+				<div class="icon-content__wrapper">
+					<span class="icon__content">v3-icon-set</span>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+<style lang="scss" scoped>
+ul {
+	list-style-type: none;
+}
+li {
+	margin: 0;
+	padding: 0;
+}
+.icon-container {
+	.icon__list {
+		// display: flex;
+		// flex-wrap: wrap;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+		width: 800px;
+	}
+	.icon__item {
+		text-align: center;
+		.icon-title__wrapper {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+			padding: 12px;
+			.icon__title {
+				font-size: 25px;
+			}
+		}
+		.icon-content__wrapper {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+			padding: 8px;
+			.icon__content {
+			}
+		}
+	}
+}
+</style>
