51f8364528b0f18ccc8b67c94d11e9dc7c8631aa
ddzy <1766083035@qq.com>
2021-04-14 11:07:27 +0800

    feat(button): 新增纯图标类型的按钮
    affects: button

--- packages/button/src/App.vue
+++ packages/button/src/App.vue
@@ -1,152 +1,5 @@
 <template>
 	<div class="button-container">
-		<!-- <div class="button__list">
-			<V3Button class="button__item" type="default">默认按钮</V3Button>
-			<V3Button class="button__item" type="info">信息按钮</V3Button>
-			<V3Button class="button__item" type="primary">主按钮</V3Button>
-			<V3Button class="button__item" type="success">成功按钮</V3Button>
-			<V3Button class="button__item" type="danger">危险按钮</V3Button>
-			<V3Button class="button__item" type="warning">警告按钮</V3Button>
-
-			<V3Button class="button__item" borderType="dashed">
-				默认按钮 + 虚线边框
-			</V3Button>
-			<V3Button class="button__item" borderType="dotted">
-				默认按钮 + 点边框
-			</V3Button>
-		</div>
-
-		<div class="button__list">
-			<V3Button class="button__item" type="default" :disabled="true">
-				默认按钮 + 禁用
-			</V3Button>
-			<V3Button class="button__item" type="info" :disabled="true">
-				信息按钮 + 禁用
-			</V3Button>
-			<V3Button class="button__item" type="primary" :disabled="true">
-				主按钮 + 禁用
-			</V3Button>
-			<V3Button class="button__item" type="success" :disabled="true">
-				成功按钮 + 禁用
-			</V3Button>
-			<V3Button class="button__item" type="danger" :disabled="true">
-				危险按钮 + 禁用
-			</V3Button>
-			<V3Button class="button__item" type="warning" :disabled="true">
-				警告按钮 + 禁用
-			</V3Button>
-
-			<V3Button class="button__item" borderType="dashed" :disabled="true">
-				默认按钮 + 虚线边框 + 禁用
-			</V3Button>
-			<V3Button class="button__item" borderType="dotted" :disabled="true">
-				默认按钮 + 点边框 + 禁用
-			</V3Button>
-		</div>
-
-		<div class="button__list">
-			<V3Button class="button__item" type="default" :plain="true">
-				默认按钮 + 朴素
-			</V3Button>
-			<V3Button class="button__item" type="info" :plain="true">
-				信息按钮 + 朴素
-			</V3Button>
-			<V3Button class="button__item" type="primary" :plain="true">
-				主按钮 + 朴素
-			</V3Button>
-			<V3Button class="button__item" type="success" :plain="true">
-				成功按钮 + 朴素
-			</V3Button>
-			<V3Button class="button__item" type="danger" :plain="true">
-				危险按钮 + 朴素
-			</V3Button>
-			<V3Button class="button__item" type="warning" :plain="true">
-				警告按钮 + 朴素
-			</V3Button>
-
-			<V3Button class="button__item" borderType="dashed" :plain="true">
-				默认按钮 + 虚线边框 + 朴素
-			</V3Button>
-			<V3Button class="button__item" borderType="dotted" :plain="true">
-				默认按钮 + 点边框 + 朴素
-			</V3Button>
-		</div>
-
-		<div class="button__list">
-			<V3Button class="button__item" type="text">文字按钮</V3Button>
-			<V3Button class="button__item" type="text" :disabled="true">
-				文字按钮 + 禁用
-			</V3Button>
-		</div>
-
-		<div class="button__list">
-			<V3Button
-				class="button__item"
-				:type="'default'"
-				:icon="'v3-icon-search'"
-				:plain="true"
-			>
-				默认按钮 + 朴素 + 图标
-			</V3Button>
-			<V3Button
-				class="button__item"
-				type="info"
-				:icon="'v3-icon-prompt'"
-				:plain="true"
-			>
-				信息按钮 + 朴素 + 图标
-			</V3Button>
-			<V3Button
-				class="button__item"
-				type="primary"
-				:icon="'v3-icon-upload'"
-				:plain="true"
-			>
-				主按钮 + 朴素 + 图标
-			</V3Button>
-			<V3Button
-				class="button__item"
-				type="success"
-				:plain="true"
-				:icon="'v3-icon-assessed-badge'"
-			>
-				成功按钮 + 朴素 + 图标
-			</V3Button>
-			<V3Button
-				class="button__item"
-				type="danger"
-				:plain="true"
-				:icon="'v3-icon-close'"
-			>
-				危险按钮 + 朴素 + 图标
-			</V3Button>
-			<V3Button
-				class="button__item"
-				type="warning"
-				:plain="true"
-				:icon="'v3-icon-warning'"
-			>
-				警告按钮 + 朴素 + 图标
-			</V3Button>
-
-			<V3Button
-				class="button__item"
-				borderType="dashed"
-				:plain="true"
-				:icon="'v3-icon-password'"
-			>
-				默认按钮 + 虚线边框 + 图标
-			</V3Button>
-			<V3Button
-				class="button__item"
-				borderType="dotted"
-				:plain="true"
-				:icon="'v3-icon-pin'"
-			>
-				默认按钮 + 点边框 + 图标
-			</V3Button>
-		</div> -->
-
 		<div class="button__list">
 			<v3-button
 				class="button__item"
@@ -234,6 +87,18 @@
 			<v3-button
 				class="button__item"
 				v-for="v in textAndIconBtnList"
+				:key="v.type"
+				:type="v.type"
+				:icon="v.icon"
+			>
+				{{ v.title }}
+			</v3-button>
+		</div>
+
+		<div class="button__list">
+			<v3-button
+				class="button__item"
+				v-for="v in iconBtnList"
 				:key="v.type"
 				:type="v.type"
 				:icon="v.icon"
@@ -456,6 +321,36 @@ export default defineComponent({
 				icon: 'v3-icon-link',
 			},
 		];
+		// 纯图标按钮
+		const iconBtnList: Array<{
+			type: string;
+			icon: string;
+		}> = [
+			{
+				type: 'default',
+				icon: 'v3-icon-search',
+			},
+			{
+				type: 'info',
+				icon: 'v3-icon-prompt',
+			},
+			{
+				type: 'primary',
+				icon: 'v3-icon-upload',
+			},
+			{
+				type: 'success',
+				icon: 'v3-icon-assessed-badge',
+			},
+			{
+				type: 'danger',
+				icon: 'v3-icon-close',
+			},
+			{
+				type: 'warning',
+				icon: 'v3-icon-warning',
+			},
+		];
 
 		return {
 			defaultBtnList,
@@ -466,6 +361,7 @@ export default defineComponent({
 			textBtnList,
 			textAndDisabledBtnList,
 			textAndIconBtnList,
+			iconBtnList
 		};
 	},
 });
