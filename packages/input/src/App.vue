b608c840a07888e1b3965fa3c3a2101ae9cb1ba0
ddzy <1766083035@qq.com>
2021-04-25 09:52:06 +0800

    WIP(input): 在 input 组件中引入 button 并简单测试
    affects: input

--- packages/input/src/App.vue
+++ packages/input/src/App.vue
@@ -1,9 +1,26 @@
-<template></template>
+<template>
+	<div class="input-demo-container">
+		<v3-button
+			type="primary"
+			icon="v3-icon-search"
+			@click="search"
+		>搜索</v3-button>
+	</div>
+</template>
 <script lang="ts">
-import { defineComponent, reactive } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-	components: {}
+	components: {},
+	setup(props, context) {
+		function search(e) {
+			console.log('e :>> ', e);
+		}
+
+		return {
+			search,
+		}
+	},
 });
 </script>
 <style lang="scss" scoped></style>
