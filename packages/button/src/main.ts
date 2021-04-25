40d5b633c530a13c1aef499cbb9282e49cc9f008
ddzy <1766083035@qq.com>
2021-04-25 08:43:50 +0800

    feat(button、input): 初始化 input 项目
    affects: button, input
    
    1. 更改 Button 组件的导出方式;
    2. 初始化 Input 项目

--- packages/button/src/main.ts
+++ packages/button/src/main.ts
@@ -1,7 +1,4 @@
-import { createApp } from 'vue';
-import App from './App.vue';
 import 'icon';
+import Button from './components/index';
 
-const app = createApp(App);
-
-app.mount('#app');
+export default Button;
---
+++ packages/input/src/App.vue
@@ -0,0 +1,9 @@
+<template></template>
+<script lang="ts">
+import { defineComponent, reactive } from 'vue';
+
+export default defineComponent({
+	components: {}
+});
+</script>
+<style lang="scss" scoped></style>
