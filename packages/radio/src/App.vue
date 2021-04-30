c5213c0ea6beb208306c90a651626019ca590641
ddzy <1766083035@qq.com>
2021-04-30 16:20:49 +0800

    feat(radio.vue): 完成 Radio 的基本样式
    affects: radio

--- packages/radio/src/App.vue
+++ packages/radio/src/App.vue
@@ -10,13 +10,31 @@
 				:prefixIcon="'v3-icon-search'"
 			></v3-input>
 		</div>
+		<div class="demo__inner">
+			<v3-button type="primary">提交</v3-button>
+		</div>
+		<div class="demo__inner">
+			<v3-button type="text">点我跳转</v3-button>
+		</div>
+		<div class="demo__inner">
+			<v3-button icon="v3-icon-editor" type="success" circle></v3-button>
+		</div>
+		<div class="demo__inner">
+			<v3-radio :label="'1'">单选1</v3-radio>
+			<v3-radio :label="'2'">单选1</v3-radio>
+			<v3-radio :label="'3'">单选1</v3-radio>
+			<v3-radio :label="'4'">单选1</v3-radio>
+		</div>
 	</div>
 </template>
 <script lang="ts">
 import { defineComponent, reactive, ref, watch } from 'vue';
+import V3Radio from './components/Radio.vue';
 
 export default defineComponent({
-	components: {},
+	components: {
+		V3Radio,
+	},
 	setup(props, context) {
 		const state = reactive({
 			inputValue: '测试值123456789',
--- packages/radio/src/components/Radio.vue
+++ packages/radio/src/components/Radio.vue
@@ -1,8 +1,29 @@
-<template> </template>
+<template>
+	<div
+		:class="{
+			'v3-radio': true,
+		}"
+	>
+		<label for="v3-radio__input" class="v3-radio__label-wrapper">
+			<input
+				id="v3-radio__input"
+				type="radio"
+				class="v3-radio__input"
+				:value="state.defaultProps.label"
+			/>
+
+			<div class="v3-radio__select"></div>
+			<div class="v3-radio__label">
+				<slot></slot>
+			</div>
+		</label>
+	</div>
+</template>
 <script lang="ts">
 import {
 	defineComponent,
 	getCurrentInstance,
+	PropType,
 	reactive,
 	toRef,
 	watch,
@@ -10,11 +31,28 @@ import {
 import * as TYPES from '../index';
 
 export default defineComponent({
-	name: 'V3Input',
-	props: {},
+	name: 'V3Radio',
+	props: {
+		border: Boolean as PropType<TYPES.IRadioBorder>,
+		disabled: Boolean as PropType<TYPES.IRadioDisabled>,
+		label: {
+			type: (Boolean || String || Number) as PropType<TYPES.IRadioLabel>,
+			validator(label) {
+				return (
+					typeof label === 'boolean' ||
+					typeof label === 'string' ||
+					typeof label === 'number'
+				);
+			},
+		},
+	},
 	setup(props, context) {
 		const state = reactive({
-			defaultProps: {},
+			defaultProps: {
+				border: false,
+				disabled: false,
+				label: '',
+			},
 		});
 		const app = getCurrentInstance();
 
@@ -37,5 +75,5 @@ export default defineComponent({
 });
 </script>
 <style lang="scss" scoped>
-@import './Input.scss';
+@import './Radio.scss';
 </style>
