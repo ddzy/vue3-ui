ba8617c582e58e92439f20d72f49439bc299bc6b
ddzy <1766083035@qq.com>
2021-05-07 10:04:28 +0800

    feat(radio): 导出 Radio 相关组件, 方便其它组件使用
    affects: radio

---
+++ packages/radio/src/components/index.ts
@@ -0,0 +1,35 @@
+import { App, Component } from 'vue';
+import V3Radio from './Radio.vue';
+import V3RadioButton from './RadioButton.vue';
+import V3RadioGroup from './RadioGroup.vue';
+
+interface ICompoent {
+	name: string;
+	component: Component;
+}
+
+/**
+ * 全局组件列表
+ */
+const globalComponentList: ICompoent[] = [
+	{
+		name: 'v3-input',
+		component: V3Radio,
+	},
+	{
+		name: 'v3-button-textarea',
+		component: V3RadioButton,
+	},
+	{
+		name: 'v3-button-number',
+		component: V3RadioGroup,
+	},
+];
+
+const install = function(app: App) {
+	globalComponentList.forEach(v => {
+		app.component(v.name, v.component);
+	});
+};
+
+export default install;
--- packages/radio/src/main.ts
+++ packages/radio/src/main.ts
@@ -1,11 +1,3 @@
-import { createApp } from 'vue';
-import V3Button from 'button';
-import V3Input from 'input';
-import App from './App.vue';
+import Radio from './components/index';
 
-const app = createApp(App);
-
-app.use(V3Button);
-app.use(V3Input);
-
-app.mount('#app');
+export default Radio;
