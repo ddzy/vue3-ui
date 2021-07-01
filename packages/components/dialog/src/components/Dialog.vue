f9d8430ec1f766c31a7a259fd3263cbd38a92daf
ddzy <1766083035@qq.com>
2021-07-01 18:09:11 +0800

    feat(Dialog.vue): 声明 Dialog 组件的所有 props 配置项
    affects: dialog

--- packages/components/dialog/src/components/Dialog.vue
+++ packages/components/dialog/src/components/Dialog.vue
@@ -2,7 +2,13 @@
 	<div class="v3-dialog">123</div>
 </template>
 <script lang="ts">
-import { defineComponent, getCurrentInstance, reactive, ref } from 'vue';
+import {
+	defineComponent,
+	getCurrentInstance,
+	PropType,
+	reactive,
+	ref,
+} from 'vue';
 import * as TYPES from '@/public/types/dialog';
 
 export default defineComponent({
@@ -12,6 +18,61 @@ export default defineComponent({
 		modelValue: {
 			type: Boolean,
 			required: true,
+		},
+		/** 弹窗的标题 */
+		title: {
+			type: String,
+			default: '',
+		},
+		/** 弹窗宽度 */
+		width: {
+			type: String,
+			default: '50%',
+		},
+		/** 弹窗整体是否水平垂直居中 */
+		center: {
+			type: Boolean,
+			default: true,
+		},
+		/** 是否全屏展示 */
+		fullscreen: {
+			type: Boolean,
+			default: false,
+		},
+		/** 是否显示遮罩层 */
+		backdrop: {
+			type: Boolean,
+			default: true,
+		},
+		/** 是否禁止滚动穿透 */
+		fixed: {
+			type: Boolean,
+			default: true,
+		},
+		/** 自定义弹窗的类名 */
+		customClass: {
+			type: String,
+			default: '',
+		},
+		/** 点击遮罩层时是否同时关闭弹窗 */
+		closeOnClickBackdrop: {
+			type: Boolean,
+			default: true,
+		},
+		/** 是否显示关闭按钮 */
+		showClose: {
+			type: Boolean,
+			default: true,
+		},
+		/** header 区域的操作按钮组 */
+		headerActions: {
+			type: Array as PropType<TYPES.IDialogHeaderActionItem[]>,
+			default: [],
+		},
+		/** 关闭弹窗前要触发的事件（执行 done 回调则表明关闭弹窗，反之则不关闭） */
+		beforeClose: {
+			type: Function as PropType<TYPES.IDialogBeforeClose>,
+			default: null,
 		},
 	},
 	setup(props: TYPES.IDialogProps, context) {
--- public/types/dialog.d.ts
+++ public/types/dialog.d.ts
@@ -1,1 +1,22 @@
-export interface IDialogProps {}
+export interface IDialogProps {
+	modelValue: boolean;
+	title: string;
+	width: string;
+	center: boolean;
+	fullscreen: boolean;
+	backdrop: boolean;
+	fixed: boolean;
+	customClass: string;
+	closeOnClickBackdrop: boolean;
+	showClose: boolean;
+	headerActions: IDialogHeaderActionItem[];
+	beforeClose: IDialogBeforeClose;
+}
+export type IDialogBeforeClose = (done: () => void) => void;
+export interface IDialogHeaderActionItem {
+	type: string;
+	text: string;
+	icon: string;
+	loading: boolean;
+	handler: (...args: any[]) => void;
+}
