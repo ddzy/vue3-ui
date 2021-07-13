dcaf0a6cbc860bdb2572435d58684672535727a2
ddzy <1766083035@qq.com>
2021-07-13 17:55:30 +0800

    feat(Tooltip.vue): 新增基本用法案例
    affects: tooltip

--- packages/components/tooltip/src/App.vue
+++ packages/components/tooltip/src/App.vue
@@ -1,6 +1,134 @@
 <template>
 	<div class="demo-container">
-		<v3-tooltip></v3-tooltip>
+		<v3-row>
+			<v3-col :span="12">
+				<h3>基本用法：</h3>
+			</v3-col>
+			<v3-col>
+				<v3-row justify="center" :gutter="20">
+					<v3-col>
+						<v3-tooltip
+							content="上左"
+							placement="top-start"
+							v-model="state.showTopStart"
+						>
+							<v3-button plain>上左</v3-button>
+						</v3-tooltip>
+					</v3-col>
+					<v3-col>
+						<v3-tooltip content="上中" placement="top" v-model="state.showTop">
+							<v3-button plain>上中</v3-button>
+						</v3-tooltip>
+					</v3-col>
+					<v3-col>
+						<v3-tooltip
+							content="上右"
+							placement="top-end"
+							v-model="state.showTopEnd"
+						>
+							<v3-button plain>上右</v3-button>
+						</v3-tooltip>
+					</v3-col>
+				</v3-row>
+				<v3-row justify="space-between" :gutter="20">
+					<v3-col>
+						<v3-row justify="space-around" :gutter="20">
+							<v3-col :span="12">
+								<v3-tooltip
+									content="左上"
+									placement="left-start"
+									v-model="state.showLeftStart"
+								>
+									<v3-button plain>左上</v3-button>
+								</v3-tooltip>
+							</v3-col>
+							<v3-col style="margin-top: 20px" :span="12">
+								<v3-tooltip
+									content="左中"
+									placement="left"
+									v-model="state.showLeft"
+								>
+									<v3-button plain>左中</v3-button>
+								</v3-tooltip>
+							</v3-col>
+							<v3-col style="margin-top: 20px" :span="12">
+								<v3-tooltip
+									content="左下"
+									placement="left-end"
+									v-model="state.showLeftEnd"
+								>
+									<v3-button plain>左下</v3-button>
+								</v3-tooltip>
+							</v3-col>
+						</v3-row>
+					</v3-col>
+					<v3-col>
+						<v3-row
+							style="text-align: right;"
+							justify="space-around"
+							:gutter="20"
+						>
+							<v3-col :span="12">
+								<v3-tooltip
+									content="右上"
+									placement="right-start"
+									v-model="state.showRightStart"
+								>
+									<v3-button plain>右上</v3-button>
+								</v3-tooltip>
+							</v3-col>
+							<v3-col style="margin-top: 20px" :span="12">
+								<v3-tooltip
+									content="右中"
+									placement="right"
+									v-model="state.showRight"
+								>
+									<v3-button plain>右中</v3-button>
+								</v3-tooltip>
+							</v3-col>
+							<v3-col style="margin-top: 20px" :span="12">
+								<v3-tooltip
+									content="右下"
+									placement="right-end"
+									v-model="state.showRightEnd"
+								>
+									<v3-button plain>右下</v3-button>
+								</v3-tooltip>
+							</v3-col>
+						</v3-row>
+					</v3-col>
+				</v3-row>
+				<v3-row justify="center" :gutter="20">
+					<v3-col>
+						<v3-tooltip
+							content="下左"
+							placement="bottom-start"
+							v-model="state.showBottomStart"
+						>
+							<v3-button plain>下左</v3-button>
+						</v3-tooltip>
+					</v3-col>
+					<v3-col>
+						<v3-tooltip
+							content="下中"
+							placement="bottom"
+							v-model="state.showBottom"
+						>
+							<v3-button plain>下中</v3-button>
+						</v3-tooltip>
+					</v3-col>
+					<v3-col>
+						<v3-tooltip
+							content="下右"
+							placement="bottom-end"
+							v-model="state.showBottomEnd"
+						>
+							<v3-button plain>下右</v3-button>
+						</v3-tooltip>
+					</v3-col>
+				</v3-row>
+			</v3-col>
+		</v3-row>
 	</div>
 </template>
 <script lang="ts">
@@ -8,7 +136,20 @@ import { defineComponent, reactive, toRef, watch } from 'vue';
 
 export default defineComponent({
 	setup(props, context) {
-		const state = reactive({});
+		const state = reactive({
+			showTop: false,
+			showTopStart: false,
+			showTopEnd: false,
+			showRight: false,
+			showRightStart: false,
+			showRightEnd: false,
+			showBottom: false,
+			showBottomStart: false,
+			showBottomEnd: false,
+			showLeft: false,
+			showLeftStart: false,
+			showLeftEnd: false,
+		});
 
 		return {
 			state,
--- packages/components/tooltip/src/components/Tooltip.vue
+++ packages/components/tooltip/src/components/Tooltip.vue
@@ -1,5 +1,37 @@
 <template>
-	<div class="v3-tooltip">tooltip</div>
+	<div
+		:class="{
+			'v3-tooltip': true,
+		}"
+	>
+		<tippy
+			:trigger="props.trigger"
+			:theme="props.theme === 'dark' ? 'material' : 'light-border'"
+			:animation="props.animation"
+			:placement="props.placement"
+			:zIndex="state.nextZIndex"
+			:arrow="false"
+			:interactive="true"
+			:allowHTML="true"
+			:delay="props.delay"
+			:offset="props.offset"
+			:onShow="handleShow"
+			:onHide="handleHide"
+			:onMount="handleMount"
+		>
+			<div class="v3-tooltip__trigger">
+				<slot></slot>
+			</div>
+			<template #content>
+				<div class="v3-tooltip__dropdown">
+					<slot v-if="context.slots.content" name="content"></slot>
+					<template v-else>
+						{{ props.content }}
+					</template>
+				</div>
+			</template>
+		</tippy>
+	</div>
 </template>
 <script lang="ts">
 import {
@@ -10,9 +42,30 @@ import {
 	ref,
 } from 'vue';
 import * as TYPES from '@/public/types/tooltip';
+import VARIABLE from '@common/constants/internal-variable';
+import { Tippy, TippyInstance } from 'vue-tippy';
+import 'tippy.js/themes/light-border.css';
+import 'tippy.js/themes/material.css';
+
+type ILocalTippyInstance =
+	| (TippyInstance & {
+			hide: () => void;
+			show: () => void;
+			unmount: () => void;
+			mount: () => void;
+	  })
+	| null;
+interface IState {
+	nextZIndex: number;
+	showDropdown: boolean;
+	tippy: ILocalTippyInstance;
+}
 
 export default defineComponent({
 	name: 'V3Tooltip',
+	components: {
+		Tippy,
+	},
 	props: {
 		modelValue: {
 			type: Boolean,
@@ -26,7 +79,7 @@ export default defineComponent({
 				return ['dark', 'light'].includes(v);
 			},
 		},
-		/** tooltip 内容，也可以通过默认 slot 传入 */
+		/** tooltip 内容，也可以通过 slot="content" 传入 */
 		content: {
 			type: String,
 			default: '',
@@ -78,18 +131,46 @@ export default defineComponent({
 		/** 触发的方式 */
 		trigger: {
 			type: String,
-			default: 'hover',
+			default: 'mouseenter',
 		},
 	},
 	setup(props: TYPES.ITooltipProps, context) {
-		const state = reactive({});
+		const state: IState = reactive({
+			/** tooltip 的 z-index（统一管理） */
+			nextZIndex: VARIABLE.getNextZIndex(),
+			/** tooltip 的显隐状态 */
+			showDropdown: false,
+			/** tippy 实例 */
+			tippy: null,
+		});
 		const app = ref(getCurrentInstance()).value;
+
+		function handleShow() {
+			// 如果当前下拉框为禁用状态，那么下拉菜单不需要显示
+			const showDropdown = !props.disabled;
+			state.showDropdown = showDropdown;
+
+			if (!showDropdown) {
+				return showDropdown;
+			}
+		}
+
+		function handleHide() {
+			state.showDropdown = false;
+		}
+
+		function handleMount(instance: ILocalTippyInstance) {
+			state.tippy = instance;
+		}
 
 		return {
 			state,
 			app,
 			props,
 			context,
+			handleShow,
+			handleHide,
+			handleMount,
 		};
 	},
 });
