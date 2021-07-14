b9fad2ba15940e25e6fb30526f8493264610bb81
ddzy <1766083035@qq.com>
2021-07-14 10:16:08 +0800

    feat(Tooltip.scss): 更改 tippy-box 的 max-width 为 100%
    affects: tooltip

--- packages/components/tooltip/src/App.vue
+++ packages/components/tooltip/src/App.vue
@@ -170,7 +170,7 @@
 				<v3-tooltip trigger="mouseenter" :offset="[0, 20]">
 					<v3-button plain>鼠标移上来</v3-button>
 					<template #content>
-						<p style="margin: 0; max-width: 200px; word-break: break-all">
+						<p style="margin: 0; width: 400px; word-break: break-all">
 							听过这样一句话，并不是每一条鱼，都生活在同一片海里。每个人都有自己的幸福阈值，如鱼饮水，冷暖自知。若用自己的经验随意去评价他人的生活，只会给彼此带来麻烦和困扰
 						</p>
 					</template>
