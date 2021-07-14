50c75c5ea9bfb37b86eb9ffa5203e491a66e1430
ddzy <1766083035@qq.com>
2021-07-14 10:11:54 +0800

    feat(App.vue): 新增[offset]配置项, 可自定义弹出菜单距离触发器的距离
    affects: tooltip

--- packages/components/tooltip/src/App.vue
+++ packages/components/tooltip/src/App.vue
@@ -161,6 +161,22 @@
 				</v3-tooltip>
 			</v3-col>
 		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>自定义弹出菜单距离触发器的距离：</h3>
+			</v3-col>
+			<v3-col>
+				<v3-tooltip trigger="mouseenter" :offset="[0, 20]">
+					<v3-button plain>鼠标移上来</v3-button>
+					<template #content>
+						<p style="margin: 0; max-width: 200px; word-break: break-all">
+							听过这样一句话，并不是每一条鱼，都生活在同一片海里。每个人都有自己的幸福阈值，如鱼饮水，冷暖自知。若用自己的经验随意去评价他人的生活，只会给彼此带来麻烦和困扰
+						</p>
+					</template>
+				</v3-tooltip>
+			</v3-col>
+		</v3-row>
 	</div>
 </template>
 <script lang="ts">
