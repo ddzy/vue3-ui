eb1125d23e9b1d761f5af023ae3cc30462ae2d1c
ddzy <1766083035@qq.com>
2021-07-15 14:21:47 +0800

    refactor(Input.scss): 调整 Input 组件的点击动画效果
    affects: input

--- packages/components/main.ts
+++ packages/components/main.ts
@@ -32,7 +32,7 @@ declare module '@vue/runtime-core' {
 }
 
 // test
-import Demo from './select/src/App.vue';
+import Demo from './input/src/App.vue';
 const app = createApp(Demo);
 app.use(install);
 app.mount('#app');
