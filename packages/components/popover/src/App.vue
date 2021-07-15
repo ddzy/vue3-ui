fe0bd0044a6f0ee6808d6cde05128708fa22c2b3
ddzy <1766083035@qq.com>
2021-07-16 07:58:00 +0800

    feat(App.vue): 完善 popover 组件的案例
    affects: popover

--- packages/components/popover/src/App.vue
+++ packages/components/popover/src/App.vue
@@ -15,6 +15,70 @@
 				</v3-popover>
 			</v3-col>
 		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>两种主题：</h3>
+			</v3-col>
+			<v3-col>
+				<v3-popover
+					content="我们都是青春曾经领养的孩子，你哭他笑，我玩着一个童年的布娃娃，一不小心跌倒，感染人生第一场抑郁，又开始学会做爱情的美梦，最后醒来时，你就突然跟身边的人发出疑问：我们什么时候长这么大的？而就在这个时候或者更早，青春不动声色地拿走了我们所有的伤疤。"
+					title="标题"
+					trigger="mouseenter"
+					theme="light"
+					placement="bottom"
+				>
+					<v3-button>明亮主题</v3-button>
+				</v3-popover>
+			</v3-col>
+			<v3-col style="margin-left: 20px">
+				<v3-popover
+					content="我们都是青春曾经领养的孩子，你哭他笑，我玩着一个童年的布娃娃，一不小心跌倒，感染人生第一场抑郁，又开始学会做爱情的美梦，最后醒来时，你就突然跟身边的人发出疑问：我们什么时候长这么大的？而就在这个时候或者更早，青春不动声色地拿走了我们所有的伤疤。"
+					title="标题"
+					trigger="mouseenter"
+					theme="dark"
+					placement="bottom"
+				>
+					<v3-button>暗黑主题</v3-button>
+				</v3-popover>
+			</v3-col>
+		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>禁用状态：</h3>
+			</v3-col>
+			<v3-col>
+				<v3-popover
+					content="我们都是青春曾经领养的孩子，你哭他笑，我玩着一个童年的布娃娃，一不小心跌倒，感染人生第一场抑郁，又开始学会做爱情的美梦，最后醒来时，你就突然跟身边的人发出疑问：我们什么时候长这么大的？而就在这个时候或者更早，青春不动声色地拿走了我们所有的伤疤。"
+					trigger="mouseenter"
+					theme="light"
+					disabled
+				>
+					<v3-button>鼠标移上来</v3-button>
+				</v3-popover>
+			</v3-col>
+		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>嵌套操作：</h3>
+			</v3-col>
+			<v3-col>
+				<v3-popover title="提示" trigger="click" theme="light">
+					<v3-button type="primary">保存</v3-button>
+					<template #content>
+						<div class="content">
+							<p>确定要保存吗？</p>
+							<div class="action">
+								<v3-button type="text">取消</v3-button>
+								<v3-button type="primary">确定</v3-button>
+							</div>
+						</div>
+					</template>
+				</v3-popover>
+			</v3-col>
+		</v3-row>
 	</div>
 </template>
 <script lang="ts">
@@ -31,4 +95,18 @@ export default defineComponent({
 	methods: {},
 });
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.content {
+	width: 180px;
+	.action {
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+		:deep(.v3-button) {
+			&:not(:first-child) {
+				margin-left: 12px;
+			}
+		}
+	}
+}
+</style>
