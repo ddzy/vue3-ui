f4459ae771fbcf27803b0728d43150cf4ff7d706
ddzy <1766083035@qq.com>
2021-07-19 09:10:19 +0800

    feat(space.d.ts): 定义 space 组件所有的可配置项
    affects: space

--- packages/components/space/src/components/Space.vue
+++ packages/components/space/src/components/Space.vue
@@ -13,7 +13,44 @@ import * as TYPES from '@/public/types/space';
 
 export default defineComponent({
 	name: 'V3Space',
-	props: {},
+	props: {
+		/** 自定义 space 容器的类名 */
+		customClass: {
+			type: String,
+			default: '',
+		},
+		/** 同 flex 布局的 align-items */
+		align: {
+			type: String as PropType<TYPES.ISpaceAlign>,
+			default: 'flex-start',
+			validator(v: string) {
+				return ['start', 'end', 'center', 'flex-start', 'flex-end'].includes(v);
+			},
+		},
+		/** 同 flex 布局的 flex-direction */
+		direction: {
+			type: String as PropType<TYPES.ISpaceDirection>,
+			default: 'row',
+			validator(v: string) {
+				return ['row', 'column', 'row-reverse', 'column-reverse'].includes(v);
+			},
+		},
+		/** 同 flex 布局的 flex-wrap */
+		wrap: {
+			type: Boolean,
+			default: true,
+		},
+		/** 自定义分隔符，默认为空格 */
+		separator: {
+			type: [String, Object] as PropType<TYPES.ISpaceSeparator>,
+			default: '',
+		},
+		/** 间隔大小 */
+		size: {
+			type: Number,
+			default: 6,
+		},
+	},
 	setup(props: TYPES.ISpaceProps, context) {
 		const state = reactive({});
 		const app = ref(getCurrentInstance()).value;
--- public/types/space.d.ts
+++ public/types/space.d.ts
@@ -1,1 +1,19 @@
-export interface ISpaceProps { }
+import { VNode } from 'vue';
+
+export type ISpaceAlign =
+	| 'start'
+	| 'end'
+	| 'center'
+	| 'flex-start'
+	| 'flex-end';
+export type ISpaceDirection = 'row' | 'column' | 'row-reverse' | 'column-reverse';
+export type ISpaceSeparator = VNode | string;
+
+export interface ISpaceProps {
+	customClass: string;
+	align: ISpaceAlign;
+  direction: ISpaceDirection;
+  wrap: boolean;
+  separator: ISpaceSeparator;
+  size: number;
+}
