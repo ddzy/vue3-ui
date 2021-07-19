6994136133a2260c67be5f5b9e67fc49b3a8a790
ddzy <1766083035@qq.com>
2021-07-19 11:35:58 +0800

    feat(App.vue): 新增[size]配置项, 可以自定义间隔大小
    affects: space

--- packages/components/space/src/App.vue
+++ packages/components/space/src/App.vue
@@ -51,6 +51,31 @@
 				</v3-space>
 			</v3-col>
 		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>控制间隔的大小：</h3>
+			</v3-col>
+			<v3-col>
+				<v3-space customClass="space-wrapper-2" wrap :size="20">
+					<v3-button type="text">按钮1</v3-button>
+					<v3-button type="text">按钮2</v3-button>
+					<v3-button type="text">按钮3</v3-button>
+					<v3-button type="text">按钮4</v3-button>
+					<v3-button type="text">按钮5</v3-button>
+					<v3-button type="text">按钮1</v3-button>
+					<v3-button type="text">按钮2</v3-button>
+					<v3-button type="text">按钮3</v3-button>
+					<v3-button type="text">按钮4</v3-button>
+					<v3-button type="text">按钮5</v3-button>
+					<v3-button type="text">按钮1</v3-button>
+					<v3-button type="text">按钮2</v3-button>
+					<v3-button type="text">按钮3</v3-button>
+					<v3-button type="text">按钮4</v3-button>
+					<v3-button type="text">按钮5</v3-button>
+				</v3-space>
+			</v3-col>
+		</v3-row>
 	</div>
 </template>
 <script lang="ts">
@@ -70,5 +95,8 @@ export default defineComponent({
 <style lang="scss" scoped>
 .space-wrapper {
 	border: 1px dashed rgba(184, 176, 176, 1);
+}
+.space-wrapper-2 {
+	width: 200px;
 }
 </style>
