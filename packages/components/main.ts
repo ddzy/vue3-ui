2ba486b27118cef6975c2e7f5b3241e07714a5d7
ddzy <1766083035@qq.com>
2021-07-21 15:11:40 +0800

    fix(Select.scss): 解决 select 组件多选时内容过长导致的背景透明的问题
    affects: select

--- packages/components/main.ts
+++ packages/components/main.ts
@@ -36,7 +36,7 @@ declare module '@vue/runtime-core' {
 }
 
 // test
-import Demo from './checkbox/src/App.vue';
+import Demo from './select/src/App.vue';
 const app = createApp(Demo);
 app.use(install);
 app.mount('#app');
