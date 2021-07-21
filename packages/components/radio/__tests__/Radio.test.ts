7765926d2fe65bc070c28b7e314af62ae056278e
ddzy <1766083035@qq.com>
2021-07-21 09:30:04 +0800

    test(Radio.test.ts): 测试 radio 组件
    affects: radio

--- packages/components/radio/__tests__/Radio.test.ts
+++ packages/components/radio/__tests__/Radio.test.ts
@@ -2,8 +2,8 @@ import { mount } from '@vue/test-utils';
 import { reactive } from 'vue';
 import V3Radio from '../src/components/Radio.vue';
 
-describe('Radio 组件测试：', () => {
-	test('Radio 组件应该正常进行值的【双向绑定】', async () => {
+describe('V3Radio 组件测试：', () => {
+	test('V3Radio 组件应该正常进行值的【双向绑定】', async () => {
 		const wrapper1 = mount({
 			template: `
         <div>
@@ -71,7 +71,7 @@ describe('Radio 组件测试：', () => {
 		expect(wrapper1.emitted()).toHaveProperty('change');
 	});
 
-	test('Radio 组件应该正常接收【border】属性，并展示为带有边框的单选按钮', async () => {
+	test('V3Radio 组件应该正常接收【border】属性，并展示为带有边框的单选按钮', async () => {
 		const wrapper1 = mount(V3Radio);
 
 		// 默认状态下，不带有边框
@@ -84,7 +84,7 @@ describe('Radio 组件测试：', () => {
 		expect(wrapper1.find('.is-bordered').exists()).toBeTruthy();
 	});
 
-	test('Radio 组件应该正常接收【disabled】属性，并禁用特定的单选按钮', async () => {
+	test('V3Radio 组件应该正常接收【disabled】属性，并禁用特定的单选按钮', async () => {
 		const wrapper1 = mount(V3Radio);
 
 		// 默认状态下，都是可用的
@@ -95,5 +95,93 @@ describe('Radio 组件测试：', () => {
 			disabled: true,
 		});
 		expect(wrapper1.find('.is-disabled').exists()).toBeTruthy();
+	});
+
+	test('V3Radio 组件可以接收【size】配置项，用来控制单选框的尺寸', async () => {
+		const wrapper = mount({
+			components: {
+				V3Radio,
+			},
+			template: `
+				<v3-radio
+					size="small"
+					v-model="radioValue1"
+					:label="1"
+					:border="true"
+					>小尺寸</v3-radio
+				>
+				<v3-radio
+					size="medium"
+					v-model="radioValue1"
+					:label="2"
+					:border="true"
+					>默认尺寸</v3-radio
+				>
+				<v3-radio
+					size="large"
+					v-model="radioValue1"
+					:label="3"
+					:border="true"
+					>大尺寸</v3-radio
+				>
+
+				<v3-radio size="small" v-model="radioValue2" :label="1"
+					>小尺寸</v3-radio
+				>
+				<v3-radio size="medium" v-model="radioValue2" :label="2"
+					>默认尺寸</v3-radio
+				>
+				<v3-radio size="large" v-model="radioValue2" :label="3"
+					>大尺寸</v3-radio
+				>
+			`,
+			data() {
+				return {
+					radioValue1: 1,
+					radioValue2: 2,
+				};
+			},
+		});
+
+		expect(wrapper.findAll('.v3-radio').length).toBe(6);
+		expect(wrapper.findAll('.v3-radio.is-size--small').length).toBe(2);
+		expect(wrapper.findAll('.v3-radio.is-size--medium').length).toBe(2);
+		expect(wrapper.findAll('.v3-radio.is-size--large').length).toBe(2);
+		expect(
+			wrapper
+				.findAll('.v3-radio')[0]
+				.classes()
+				.includes('is-size--small')
+		).toBeTruthy();
+		expect(
+			wrapper
+				.findAll('.v3-radio')[1]
+				.classes()
+				.includes('is-size--medium')
+		).toBeTruthy();
+		expect(
+			wrapper
+				.findAll('.v3-radio')[2]
+				.classes()
+				.includes('is-size--large')
+		).toBeTruthy();
+		expect(
+			wrapper
+				.findAll('.v3-radio')[3]
+				.classes()
+				.includes('is-size--small')
+		).toBeTruthy();
+		expect(
+			wrapper
+				.findAll('.v3-radio')[4]
+				.classes()
+				.includes('is-size--medium')
+		).toBeTruthy();
+		expect(
+			wrapper
+				.findAll('.v3-radio')[5]
+				.classes()
+				.includes('is-size--large')
+		).toBeTruthy();
 	});
 });
