e701900890c582e10bbfeb153b5ffb8cda999d32
ddzy <1766083035@qq.com>
2021-07-22 11:55:23 +0800

    feat(App.vue): 完善 message 组件的示例
    affects: message

--- packages/components/message/src/App.vue
+++ packages/components/message/src/App.vue
@@ -1,59 +1,116 @@
 <template>
 	<div class="demo-container">
-		<v3-button
-			type="success"
-			:loading="state.isSuccessBtnLoading"
-			@click="submitSuccess"
-			>成功的消息提示</v3-button
-		>
+		<v3-row>
+			<v3-col :span="12">
+				<h3>基本用法：</h3>
+			</v3-col>
+			<v3-col>
+				<v3-space :size="12">
+					<v3-button type="success" @click="handleClick1('success')"
+						>成功</v3-button
+					>
+					<v3-button type="danger" @click="handleClick1('danger')"
+						>失败</v3-button
+					>
+					<v3-button type="warning" @click="handleClick1('warning')"
+						>警告</v3-button
+					>
+					<v3-button type="info" @click="handleClick1('info')">提示</v3-button>
+				</v3-space>
+			</v3-col>
+		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>内容可以是 VNode：</h3>
+			</v3-col>
+			<v3-col>
+				<v3-space :size="12">
+					<v3-button type="success" @click="handleClick2">提示</v3-button>
+				</v3-space>
+			</v3-col>
+		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>可以关闭：</h3>
+			</v3-col>
+			<v3-col>
+				<v3-space :size="12">
+					<v3-button type="success" @click="handleClick3">成功</v3-button>
+				</v3-space>
+			</v3-col>
+		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>文本居中：</h3>
+			</v3-col>
+			<v3-col>
+				<v3-space :size="12">
+					<v3-button type="success" @click="handleClick4">成功</v3-button>
+				</v3-space>
+			</v3-col>
+		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>传入 HTML 片段：</h3>
+			</v3-col>
+			<v3-col>
+				<v3-space :size="12">
+					<v3-button type="success" @click="handleClick5">成功</v3-button>
+				</v3-space>
+			</v3-col>
+		</v3-row>
 	</div>
-	<v3-row>
-		<v3-col>
-			<v3-button type="danger" @click="submitFail">错误的消息提示</v3-button>
-		</v3-col>
-	</v3-row>
 </template>
 <script lang="ts">
-import { defineComponent, reactive } from 'vue';
-import V3Message from '@components/message/main';
+import { defineComponent, h, reactive } from 'vue';
+import { IMessageType } from '@/public/types/message';
 
 export default defineComponent({
 	setup() {
-		const state = reactive({
-			isSuccessBtnLoading: false,
-		});
+		const state = reactive({});
 
 		return {
 			state,
 		};
 	},
 	methods: {
-		submitSuccess() {
-			const instance = this.$message({
-				type: 'success',
-				duration: 2000,
-				showClose: true,
-				center: false,
-				dangerouslyUseHTMLString: true,
-				icon: '',
-				offset: 10,
-				customClass: `message-1 message-2  message-3 message-4    `,
-				closeOnHover: false,
-				onClose() {
-					return new Promise(resolve => {
-						setTimeout(() => {
-							resolve(true);
-						}, 1000);
-					});
-				},
-				message: `
-					修改成功！
-				`,
+		handleClick1(type: IMessageType) {
+			this.$message({
+				type,
+				message: type,
 			});
 		},
-		submitFail() {
-			V3Message.danger({
-				message: '修改失败！',
+		handleClick2() {
+			this.$message({
+				type: 'success',
+				message: h('span', '内容可以是 VNode~'),
+			});
+		},
+		handleClick3() {
+			this.$message({
+				type: 'success',
+				message: '可以手动关闭',
+				showClose: true,
+			});
+		},
+		handleClick4() {
+			this.$message({
+				type: 'success',
+				message: '文本居中',
+				center: true,
+			});
+		},
+		handleClick5() {
+			this.$message({
+				type: 'success',
+				message: `
+					<p>可以传入 <mark>HTML</mark> 片段~</p>
+				`,
+				dangerouslyUseHTMLString: true,
 			});
 		},
 	},
