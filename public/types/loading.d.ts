f024b5da868b5f12068e6c6d75276b4f7651226d
ddzy <1766083035@qq.com>
2021-07-23 09:47:42 +0800

    feat(loading.d.ts): 定义通过服务的方式调用 loading 时的接口

--- public/types/loading.d.ts
+++ public/types/loading.d.ts
@@ -5,5 +5,7 @@ export interface ILoadingProps {}
 export interface ILoadingConstructor {
 	(options: ILoadingProps): ILoadingConstructorReturn;
 
+	// 服务方式调用
+	service: (options: ILoadingProps) => ILoadingConstructorReturn;
 	install: (app: App) => void;
 }
