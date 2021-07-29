61e2963fe13d1116e00be676ce702df87f5b3a4f
ddzy <1766083035@qq.com>
2021-07-29 18:18:58 +0800

    feat(App.vue): 完善 popconfirm 组件的案例
    affects: popconfirm

--- packages/components/popconfirm/src/App.vue
+++ packages/components/popconfirm/src/App.vue
@@ -21,6 +21,65 @@
 				</v3-popconfirm>
 			</v3-col>
 		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>自定义图标和图标颜色：</h3>
+			</v3-col>
+			<v3-col>
+				<v3-popconfirm
+					content="要隐藏当前文章吗？"
+					trigger="click"
+					icon="v3-icon-Notvisible"
+					iconColor="rgba(201, 27, 38, 1)"
+					placement="bottom"
+				>
+					<v3-button
+						type="primary"
+						circle
+						icon="v3-icon-Notvisible"
+					></v3-button>
+				</v3-popconfirm>
+			</v3-col>
+		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>自定义按钮的类型、大小、文本内容：</h3>
+			</v3-col>
+			<v3-col>
+				<v3-space :size="12">
+					<v3-popconfirm
+						content="确定要删除吗？"
+						trigger="click"
+						placement="bottom"
+						confirmBtnType="success"
+						cancelBtnType="default"
+					>
+						<v3-button type="primary">类型</v3-button>
+					</v3-popconfirm>
+
+					<v3-popconfirm
+						content="确定要删除吗？"
+						trigger="click"
+						placement="bottom"
+						confirmBtnSize="small"
+					>
+						<v3-button type="primary">大小</v3-button>
+					</v3-popconfirm>
+
+					<v3-popconfirm
+						content="确定要删除吗？"
+						trigger="click"
+						placement="bottom"
+						cancelBtnText="不用了"
+						confirmBtnText="继续"
+					>
+						<v3-button type="primary">内容</v3-button>
+					</v3-popconfirm>
+				</v3-space>
+			</v3-col>
+		</v3-row>
 	</div>
 </template>
 <script lang="ts">
