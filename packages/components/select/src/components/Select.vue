c14b5a3584778e81b6e2d4f780f9597cc9dcb7ac
ddzy <1766083035@qq.com>
2021-08-18 18:14:36 +0800

    feat(Select.vue): 更新 select 组件, 当点击标签容器时, 显示下拉菜单; 并且当关闭标签时, 不需要关闭下拉菜单
    affects: select

--- packages/components/select/src/components/Select.vue
+++ packages/components/select/src/components/Select.vue
@@ -393,8 +393,6 @@ export default defineComponent({
 		function handleTagClose(
 			data: Pick<TYPES.ISelectOptionProps, 'label' | 'value'>
 		) {
-			state.showDropdown = false;
-
 			// 关闭标签时，从已选中的列表中移除该项
 			state.selectedOptionList = state.selectedOptionList.filter(v => {
 				return v.label !== data.label && v.value !== data.value;
