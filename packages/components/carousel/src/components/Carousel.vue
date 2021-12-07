e99cf5f97292aa4a05a689b40c6919d0b3b2ead5
ddzy <1766083035@qq.com>
2021-12-07 15:01:22 +0800

    feat(carousel.d.ts): 预定义轮播图所有的可配置项
    affects: carousel

--- packages/components/carousel/src/components/Carousel.vue
+++ packages/components/carousel/src/components/Carousel.vue
@@ -12,7 +12,74 @@ interface IState {}
 export default defineComponent({
 	name: 'V3Carousel',
 	components: {},
-	props: {},
+	props: {
+		modelValue: {
+			type: Number,
+			default: 0,
+		},
+		/** 轮播图形式 */
+		effect: {
+			type: String as PropType<TYPES.ICarouselEffect>,
+			default: 'slide',
+			validator(v: string) {
+				return ['slide', 'fade'].includes(v);
+			},
+		},
+		/** 是否自动播放 */
+		autoplay: {
+			type: Boolean,
+			default: true,
+		},
+		/** 鼠标悬浮于轮播图区域上方时，是否停止自动切换 */
+		pauseOnHover: {
+			type: Boolean,
+			default: true,
+		},
+		/** 同 CSS 属性 transition-duration */
+		duration: {
+			type: Number,
+			default: 600,
+		},
+		/** 同 CSS 属性 transition-timing-function */
+		timingFunction: {
+			type: String,
+			default: 'cubic-bezier(0.17, 0.84, 0.44, 1)',
+		},
+		/** 是否禁用轮播图（不响应一切事件和切换动作） */
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+		/** 轮播图的方向 */
+		direction: {
+			type: String as PropType<TYPES.ICarouselDirection>,
+			default: 'horizontal',
+			validator(v: string) {
+				return ['horizontal', 'vertical'].includes(v);
+			},
+		},
+		/** 是否显示切换箭头 */
+		showArrow: {
+			type: String as PropType<TYPES.ICarouselShowArrow>,
+			default: 'always',
+			validator(v: string) {
+				return ['always', 'hover', 'never'].includes(v);
+			},
+		},
+		/** 是否显示导航按钮 */
+		showIndicator: {
+			type: Boolean,
+			default: true,
+		},
+		/** 导航按钮的类型 */
+		indicatorType: {
+			type: String as PropType<TYPES.ICarouselIndicatorType>,
+			default: 'dot',
+			validator(v: string) {
+				return ['dot', 'line'].includes(v);
+			},
+		},
+	},
 	setup(props: TYPES.ICarouselProps, context) {
 		const state: IState = reactive({});
 
--- public/types/carousel.d.ts
+++ public/types/carousel.d.ts
@@ -1,1 +1,18 @@
-export interface ICarouselProps {}
+export type ICarouselEffect = 'fade' | 'slide';
+export type ICarouselDirection = 'horizontal' | 'vertical';
+export type ICarouselShowArrow = 'always' | 'hover' | 'never';
+export type ICarouselIndicatorType = 'dot' | 'line';
+
+export interface ICarouselProps {
+	modelValue: number;
+	effect: ICarouselEffect;
+  autoplay: boolean;
+  pauseOnHover: boolean;
+	duration: number;
+	timingFunction: string;
+	disabled: boolean;
+	direction: ICarouselDirection;
+	showArrow: ICarouselShowArrow;
+	showIndicator: boolean;
+	indicatorType: ICarouselIndicatorType;
+}
