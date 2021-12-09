c2460cdf22ba6a8fe2ff335732e931903152f529
ddzy <1766083035@qq.com>
2021-12-09 14:55:43 +0800

    feat(App.vue): 新增[duration、timingFunction]配置项, 可以自定义切换时的过渡时间和过渡曲线
    affects: carousel

--- packages/components/carousel/src/App.vue
+++ packages/components/carousel/src/App.vue
@@ -28,7 +28,7 @@
 			</v3-col>
 			<v3-col>
 				<v3-carousel
-					v-model="state.carouselIndex1"
+					v-model="state.carouselIndex2"
 					effect="fade"
 					:width="500"
 					:height="300"
@@ -53,7 +53,7 @@
 			</v3-col>
 			<v3-col>
 				<v3-carousel
-					v-model="state.carouselIndex1"
+					v-model="state.carouselIndex3"
 					effect="slide"
 					:width="500"
 					:height="300"
@@ -89,7 +89,7 @@
 
 		<v3-row>
 			<v3-col :span="12">
-				<h3>切换箭头的显示时机：</h3>
+				<h3>自定义箭头的显示时机：</h3>
 			</v3-col>
 			<v3-col>
 				<div style="width: 500px; height: 300px">
@@ -104,6 +104,27 @@
 								backgroundPosition: 'center center',
 							}"
 						></v3-carousel-item>
+					</v3-carousel>
+				</div>
+			</v3-col>
+		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>自定义切换时的过渡时间、过渡曲线</h3>
+			</v3-col>
+			<v3-col>
+				<div
+					style="width: 500px; height: 300px; border: 1px solid rgba(221, 221, 221, 1)"
+				>
+					<v3-carousel :duration="300" :timingFunction="'ease'">
+						<v3-carousel-item v-for="(v, i) in state.carouselList1" :key="v.id">
+							<div
+								style="display: flex; justify-content: center; align-items: center; height: 100%;"
+							>
+								<p style="font-size: 30px; font-weight: bold">Slide {{ i }}</p>
+							</div>
+						</v3-carousel-item>
 					</v3-carousel>
 				</div>
 			</v3-col>
@@ -139,6 +160,8 @@ export default defineComponent({
 				},
 			],
 			carouselIndex1: 0,
+			carouselIndex2: 0,
+			carouselIndex3: 0,
 		});
 
 		onMounted(() => {
