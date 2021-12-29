9adc427068cea15153280c53e0c65674477ce79c
ddzy <1766083035@qq.com>
2021-12-29 08:58:33 +0800

    feat(App.vue): 更新 DemoBlock 组件的 demo 文件
    affects: demo-block

--- packages/components/demo-block/src/App.vue
+++ packages/components/demo-block/src/App.vue
@@ -1,35 +1,140 @@
 <template>
 	<div class="demo-container">
-		<div class="demo__item">
-			<v3-demo-block
-				:descriptionTip="'概述'"
-				:extraList="state.extraList"
-				@extraClick="handleExtraItemClick"
-			>
-				<template #default>
-					<h3 style="text-align: center; margin: 0;">标题</h3>
-				</template>
-				<template #description>
-					按钮有六种类型，分别是【默认】、【主要】、【危险】、【成功】、【警告】、【信息】
-				</template>
-				<template #detail>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-					veritatis quidem cum unde dolorum quia enim illum, aspernatur odio
-					accusantium deserunt autem ex doloribus maxime voluptatem, cupiditate
-					animi explicabo? Sed! Lorem ipsum dolor sit amet consectetur
-					adipisicing elit. Placeat veritatis molestiae quaerat eaque aliquam
-					neque voluptatem accusantium corporis voluptas quidem at totam
-					voluptate qui illo pariatur facilis, rerum doloremque ea.
-					Necessitatibus iure pariatur eligendi dolorem vitae adipisci quo hic
-					aut iusto corrupti quaerat repudiandae, voluptas corporis, voluptate,
-					aliquid dolorum commodi exercitationem ex enim nisi assumenda minus ab
-					aperiam. Dolore, voluptatibus. Ut consectetur incidunt minus facere
-					eligendi fugiat id, distinctio magni autem dolor eveniet dolores nisi
-					saepe quidem numquam facilis dolore quam. Iste officia laborum,
-					adipisci sed perferendis labore cumque aperiam?
-				</template>
-			</v3-demo-block>
-		</div>
+		<v3-row>
+			<v3-col :span="12">
+				<h3>基本使用：</h3>
+			</v3-col>
+			<v3-col :span="12">
+				<v3-demo-block>
+					<template #default>
+						<h4 style="text-align: center; margin: 0;">标题</h4>
+					</template>
+					<template #description>
+						这是一段描述信息，这是一段描述信息，这是一段描述信息
+					</template>
+					<template #code>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+						veritatis quidem cum unde dolorum quia enim illum, aspernatur odio
+						accusantium deserunt autem ex doloribus maxime voluptatem,
+						cupiditate animi explicabo? Sed! Lorem ipsum dolor sit amet
+						consectetur adipisicing elit. Placeat veritatis molestiae quaerat
+						eaque aliquam neque voluptatem accusantium corporis voluptas quidem
+						at totam voluptate qui illo pariatur facilis, rerum doloremque ea.
+						Necessitatibus iure pariatur eligendi dolorem vitae adipisci quo hic
+						aut iusto corrupti quaerat repudiandae, voluptas corporis,
+						voluptate, aliquid dolorum commodi exercitationem ex enim nisi
+						assumenda minus ab aperiam. Dolore, voluptatibus. Ut consectetur
+						incidunt minus facere eligendi fugiat id, distinctio magni autem
+						dolor eveniet dolores nisi saepe quidem numquam facilis dolore quam.
+						Iste officia laborum, adipisci sed perferendis labore cumque
+						aperiam?
+					</template>
+				</v3-demo-block>
+			</v3-col>
+		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>自定义描述区域的提示：</h3>
+			</v3-col>
+			<v3-col :span="12">
+				<v3-demo-block :descriptionTip="'简介'">
+					<template #default>
+						<h4 style="text-align: center; margin: 0;">标题</h4>
+					</template>
+					<template #description>
+						这是一段描述信息，这是一段描述信息，这是一段描述信息
+					</template>
+					<template #code>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+						veritatis quidem cum unde dolorum quia enim illum, aspernatur odio
+						accusantium deserunt autem ex doloribus maxime voluptatem,
+						cupiditate animi explicabo? Sed! Lorem ipsum dolor sit amet
+						consectetur adipisicing elit. Placeat veritatis molestiae quaerat
+						eaque aliquam neque voluptatem accusantium corporis voluptas quidem
+						at totam voluptate qui illo pariatur facilis, rerum doloremque ea.
+						Necessitatibus iure pariatur eligendi dolorem vitae adipisci quo hic
+						aut iusto corrupti quaerat repudiandae, voluptas corporis,
+						voluptate, aliquid dolorum commodi exercitationem ex enim nisi
+						assumenda minus ab aperiam. Dolore, voluptatibus. Ut consectetur
+						incidunt minus facere eligendi fugiat id, distinctio magni autem
+						dolor eveniet dolores nisi saepe quidem numquam facilis dolore quam.
+						Iste officia laborum, adipisci sed perferendis labore cumque
+						aperiam?
+					</template>
+				</v3-demo-block>
+			</v3-col>
+		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>自定义伸缩按钮的文本：</h3>
+			</v3-col>
+			<v3-col :span="12">
+				<v3-demo-block defaultBtnText="展开" expandedBtnText="收起">
+					<template #default>
+						<h4 style="text-align: center; margin: 0;">标题</h4>
+					</template>
+					<template #description>
+						这是一段描述信息，这是一段描述信息，这是一段描述信息
+					</template>
+					<template #code>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+						veritatis quidem cum unde dolorum quia enim illum, aspernatur odio
+						accusantium deserunt autem ex doloribus maxime voluptatem,
+						cupiditate animi explicabo? Sed! Lorem ipsum dolor sit amet
+						consectetur adipisicing elit. Placeat veritatis molestiae quaerat
+						eaque aliquam neque voluptatem accusantium corporis voluptas quidem
+						at totam voluptate qui illo pariatur facilis, rerum doloremque ea.
+						Necessitatibus iure pariatur eligendi dolorem vitae adipisci quo hic
+						aut iusto corrupti quaerat repudiandae, voluptas corporis,
+						voluptate, aliquid dolorum commodi exercitationem ex enim nisi
+						assumenda minus ab aperiam. Dolore, voluptatibus. Ut consectetur
+						incidunt minus facere eligendi fugiat id, distinctio magni autem
+						dolor eveniet dolores nisi saepe quidem numquam facilis dolore quam.
+						Iste officia laborum, adipisci sed perferendis labore cumque
+						aperiam?
+					</template>
+				</v3-demo-block>
+			</v3-col>
+		</v3-row>
+
+		<v3-row>
+			<v3-col :span="12">
+				<h3>自定义功能按钮：</h3>
+			</v3-col>
+			<v3-col :span="12">
+				<v3-demo-block
+					:descriptionTip="'简介'"
+					:extraList="state.extraList"
+					@extraClick="handleExtraItemClick"
+				>
+					<template #default>
+						<h4 style="text-align: center; margin: 0;">标题</h4>
+					</template>
+					<template #description>
+						这是一段描述信息，这是一段描述信息，这是一段描述信息
+					</template>
+					<template #code>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+						veritatis quidem cum unde dolorum quia enim illum, aspernatur odio
+						accusantium deserunt autem ex doloribus maxime voluptatem,
+						cupiditate animi explicabo? Sed! Lorem ipsum dolor sit amet
+						consectetur adipisicing elit. Placeat veritatis molestiae quaerat
+						eaque aliquam neque voluptatem accusantium corporis voluptas quidem
+						at totam voluptate qui illo pariatur facilis, rerum doloremque ea.
+						Necessitatibus iure pariatur eligendi dolorem vitae adipisci quo hic
+						aut iusto corrupti quaerat repudiandae, voluptas corporis,
+						voluptate, aliquid dolorum commodi exercitationem ex enim nisi
+						assumenda minus ab aperiam. Dolore, voluptatibus. Ut consectetur
+						incidunt minus facere eligendi fugiat id, distinctio magni autem
+						dolor eveniet dolores nisi saepe quidem numquam facilis dolore quam.
+						Iste officia laborum, adipisci sed perferendis labore cumque
+						aperiam?
+					</template>
+				</v3-demo-block>
+			</v3-col>
+		</v3-row>
 	</div>
 </template>
 <script lang="ts">
@@ -37,12 +142,7 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
 	setup() {
-		const json = {
-			a: 1,
-			b: 2,
-		};
 		const state = {
-			code: `${JSON.stringify(json, null, 2)}`,
 			extraList: [
 				{
 					title: '转发',
@@ -60,8 +160,8 @@ export default defineComponent({
 		};
 	},
 	methods: {
-		handleExtraItemClick(v: any) {
-			console.log('v :>> ', v);
+		handleExtraItemClick(v: { title: string; icon: string }) {
+			alert(v.title);
 		},
 	},
 });
