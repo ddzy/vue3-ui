61b0e9cf53a6e0de0595d65d448c5667778b660f
ddzy <1766083035@qq.com>
2022-06-23 10:42:05 +0800

    docs(loading.md): 完善 loading 组件示例文档
    affects: loading

--- packages/components/router.ts
+++ packages/components/router.ts
@@ -21,7 +21,7 @@ import V3PopoverDemo from './popover/popover.md';
 import V3DividerDemo from './divider/divider.md';
 import V3SpaceDemo from './space/space.md';
 import V3BadgeDemo from './badge/badge.md';
-import V3LoadingDemo from './loading/src/App.vue';
+import V3LoadingDemo from './loading/loading.md';
 import V3CardDemo from './card/src/App.vue';
 import V3BasePopperDemo from './base-popper/src/App.vue';
 import V3PopconfirmDemo from './popconfirm/src/App.vue';
