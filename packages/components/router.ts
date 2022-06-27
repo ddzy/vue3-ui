39bcb8d6df86660a61c0f6ffc3544cb99db85f2b
ddzy <1766083035@qq.com>
2022-06-27 17:07:37 +0800

    docs(slider.md): 完善 slider 组件的示例文档
    affects: slider

--- packages/components/router.ts
+++ packages/components/router.ts
@@ -25,7 +25,7 @@ import V3LoadingDemo from './loading/loading.md';
 import V3CardDemo from './card/card.md';
 import V3BasePopperDemo from './base-popper/base-popper.md';
 import V3PopconfirmDemo from './popconfirm/src/App.vue';
-import V3SliderDemo from './slider/src/App.vue';
+import V3SliderDemo from './slider/slider.md';
 import V3ProgressDemo from './progress/src/App.vue';
 import V3CarouselDemo from './carousel/src/App.vue';
 import V3DrawerDemo from './drawer/drawer.md';
