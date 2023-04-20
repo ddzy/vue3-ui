35437b3d05f63bb8d579df5908a732124824f1be
ddzy <1766083035@qq.com>
2023-04-20 09:46:22 +0800

    chore(*): 打包代码合并优化

--- build/lib.build.ts
+++ build/lib.build.ts
@@ -1,18 +1,42 @@
 import * as path from 'path';
+import * as fse from 'fs-extra';
 import { InlineConfig, build, mergeConfig } from 'vite';
-import commonConfig from '../config/common';
+// import commonConfig from '../config/common';
 import vue from '@vitejs/plugin-vue';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
-const baseBuildOptions: InlineConfig = {
+const commonConfig = {
+	resolve: {
+		alias: [
+			{
+				find: '@common',
+				replacement: path.resolve(__dirname, '/packages/common'),
+			},
+			{
+				find: '@components',
+				replacement: path.resolve(__dirname, '/packages/components'),
+			},
+			{
+				find: '@typings',
+				replacement: path.resolve(__dirname, '/public/lib/typings'),
+			},
+		],
+	},
+	server: {
+		open: true,
+		port: 8886,
+	},
+	css: {
+		preprocessorOptions: {
+			/** 配置 scss 全局变量的引入方式 */
+			scss: {
+				additionalData: `@import "./packages/common/styles/variable";\n@import "./packages/common/styles/reset";`,
+			},
+		},
+	},
 	// 由于采用手动打包，所以禁止自动寻找 vite.config.ts 配置文件，避免打包失败
 	configFile: false,
-	publicDir: 'public/lib',
 	build: {
-		lib: {
-			entry: path.resolve(__dirname, '../packages/components/main.lib.ts'),
-			name: 'Vue3UI',
-		},
-		outDir: `dist`,
 		rollupOptions: {
 			external: ['vue'],
 			output: {
@@ -29,38 +53,101 @@ const baseBuildOptions: InlineConfig = {
 			},
 		},
 	},
-	plugins: [
-		vue(),
-	],
-};
-  
-// index.full.js
-const buildOptionsOfFull = mergeConfig(commonConfig, mergeConfig(baseBuildOptions, {
-	build: {
-		// 是否清空 dist 文件夹
-		emptyOutDir: true,
-		lib: {
-			fileName: 'index.full',
-		},
-		minify: false,
-	},
-} as InlineConfig));
+}
 
-// index.min.js
-const buildOptionsOfMin = mergeConfig(commonConfig, mergeConfig(baseBuildOptions, {
-	build: {
-		// 是否清空 dist 文件夹
-		emptyOutDir: false,
-		lib: {
-			fileName: 'index.min',
+async function buildAll() {
+	const baseBuildOptions: InlineConfig = {
+		publicDir: 'public/lib',
+		build: {
+			sourcemap: true,
+			outDir: 'dist',
+			lib: {
+				entry: path.resolve(__dirname, '../packages/components/main.lib.ts'),
+				name: 'Vue3UI',
+			},
 		},
-		minify: 'esbuild',
-	},
-} as InlineConfig));
+		plugins: [
+			vue(),
+		],
+	}
 
-async function buildHelper() {
+	// index.full.js
+	const buildOptionsOfFull = mergeConfig(commonConfig, mergeConfig(baseBuildOptions, {
+		build: {
+			// 是否清空 dist 文件夹
+			emptyOutDir: true,
+			lib: {
+				fileName: 'index.full',
+			},
+			minify: false,
+		},
+	} as InlineConfig));
+
+	// index.min.js
+	const buildOptionsOfMin = mergeConfig(commonConfig, mergeConfig(baseBuildOptions, {
+		build: {
+			// 是否清空 dist 文件夹
+			emptyOutDir: false,
+			lib: {
+				fileName: 'index.min',
+			},
+			minify: 'esbuild',
+		},
+	} as InlineConfig));
+
 	await build(buildOptionsOfFull);
 	await build(buildOptionsOfMin);
 }
+async function buildEach() {
+	/**
+ * https://github.com/vitejs/vite/discussions/1736
+ */
+	const packagePath = path.resolve(__dirname, '../packages/components');
 
-buildHelper();
+	fse.readdirSync(packagePath, {
+		withFileTypes: true,
+	})
+		.filter(v => v.isDirectory())
+		.forEach(async v => {
+			const baseBuildOptions: InlineConfig = {
+				publicDir: false,
+				plugins: [vue(), cssInjectedByJsPlugin()],
+				build: {
+					sourcemap: false,
+					lib: {
+						entry: path.resolve(packagePath, v.name, 'main.ts'),
+						name: v.name.replace(/^.{1}/, $1 => {
+							return $1.toUpperCase();
+						}),
+					},
+					outDir: `dist/packages/${v.name}`,
+					cssCodeSplit: false,
+				},
+			};
+
+			const buildOptions = mergeConfig(commonConfig, mergeConfig(baseBuildOptions, {
+				build: {
+					minify: false,
+					emptyOutDir: true,
+					lib: {
+						fileName: 'index',
+					},
+				},
+			} as InlineConfig))
+
+			await build(buildOptions);
+
+			// 打包之后将各个包的源码复制到输出目录中
+			try {
+				fse.copySync(
+					path.resolve(packagePath, v.name, 'lib'),
+					path.resolve(__dirname, `../dist/packages/${v.name}/lib`),
+					{ overwrite: true }
+				);
+			} catch (error) { }
+		});
+
+}
+
+buildAll();
+buildEach();
