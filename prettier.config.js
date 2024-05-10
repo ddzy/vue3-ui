9f3c52fc294371eaedce5c2dcbdad7612249e51d
ddzy <1766083035@qq.com>
2024-05-10 11:19:59 +0800

    build(prettier.config.js): 更改 prettier 配置

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,21 @@
 module.exports = {
 	eslintIntegration: true,
-	semi: true, // 是否加分号
+	// 是否加分号
+	semi: true,
 	endOfLine: 'auto',
-	tabWidth: 2, // Tab 缩进的长度
-	useTabs: true, // 是否使用 Tab 缩进
-	arrowParens: 'avoid', // 箭头函数只有一个参数时，是否书写括号
-	singleQuote: true, // 是否采用单引号
-	jsxBracketSameLine: true,
-	htmlWhitespaceSensitivity: 'css', // HTML 元素的换行处理
-	trailingComma: 'es5',
-}
+	// Tab 缩进的长度
+	tabWidth: 2,
+	// 是否使用 Tab 缩进
+	useTabs: true,
+	// 箭头函数只有一个参数时，是否书写括号
+	arrowParens: 'always',
+	// https://prettier.io/docs/en/options#bracket-line
+	// jsx 开标签的闭合符号是否与属性写在同一行
+	bracketSameLine: false,
+	// 是否采用单引号
+	singleQuote: true,
+	// HTML 元素的换行处理
+	htmlWhitespaceSensitivity: 'css',
+	// 尾随逗号
+	trailingComma: 'all',
+};
