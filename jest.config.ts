74e39305e6b57095e8ac2d5221d6670eb159d696
ddzy <1766083035@qq.com>
2024-05-22 14:47:37 +0800

    test(Checkbox): Checkbox 组件单元测试

--- jest.config.ts
+++ jest.config.ts
@@ -23,7 +23,7 @@ export default {
 		],
 	},
 	// testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
-	testRegex: '/__tests__/Carousel\\.test\\.ts$',
+	testRegex: '/__tests__/Checkbox\\.test\\.ts$',
 	testPathIgnorePatterns: ['Select\\.test\\.ts'],
 	moduleFileExtensions: ['vue', 'js', 'ts'],
 	moduleNameMapper: {
