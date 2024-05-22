5994faacf84437e6ae3febc152f40130d811cc6c
ddzy <1766083035@qq.com>
2024-05-22 14:32:15 +0800

    build(package.json): 新增 jest-jasmine2 包, 替代 jest 默认的 runner

--- jest.config.ts
+++ jest.config.ts
@@ -23,7 +23,7 @@ export default {
 		],
 	},
 	// testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
-	testRegex: '/__tests__/ButtonGroup\\.test\\.ts$',
+	testRegex: '/__tests__/Carousel\\.test\\.ts$',
 	testPathIgnorePatterns: ['Select\\.test\\.ts'],
 	moduleFileExtensions: ['vue', 'js', 'ts'],
 	moduleNameMapper: {
@@ -36,4 +36,5 @@ export default {
 	testEnvironmentOptions: {
 		customExportConditions: ['node', 'node-addons'],
 	},
+	testRunner: 'jest-jasmine2',
 } as Config;
