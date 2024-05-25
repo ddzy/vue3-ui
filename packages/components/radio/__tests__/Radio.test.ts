a69bf1b4be91d5854d8ac2a592da3b72d3b11f4b
ddzy <1766083035@qq.com>
2024-05-25 14:57:51 +0800

    test(Radio): Radio 组件单元测试

--- packages/components/radio/__tests__/Radio.test.ts
+++ packages/components/radio/__tests__/Radio.test.ts
@@ -1,100 +1,32 @@
 import { mount } from '@vue/test-utils';
 import { reactive } from 'vue';
-import { V3Radio } from '@components/main';
+import { V3Input, V3Radio } from '@components/main';
 
 describe('V3Radio 组件测试：', () => {
-	test('V3Radio 组件应该正常进行值的【双向绑定】', async () => {
-		const wrapper1 = mount({
-			template: `
-        <div>
-          <v3-radio
-          v-model="state.radioValue"
-          :label="1"
-          @change="handleChange"
-          >单选1</v3-radio
-          >
-          <v3-radio
-            v-model="state.radioValue"
-            :label="2"
-            :disabled="true"
-            @change="handleChange"
-            >单选2</v3-radio
-          >
-          <v3-radio
-            v-model="state.radioValue"
-            :label="3"
-            @change="handleChange"
-            >单选3</v3-radio
-          >
-          <v3-radio
-            v-model="state.radioValue"
-            :label="4"
-            @change="handleChange"
-            >单选4</v3-radio
-          >
-        </div>
-      `,
-			components: {
-				V3Radio,
-			},
-			emits: ['change'],
-			setup(props, context) {
-				const state = reactive({
-					radioValue: 3,
-				});
-
-				function handleChange(v: MouseEvent) {
-					context.emit('change', v);
-				}
-
-				return {
-					state,
-					handleChange,
-				};
-			},
-		});
-
-		const radioList = wrapper1.findAll('.v3-radio');
-
-		// 双向绑定同一个变量的所有单选框，同一时间只能选中一个
-		expect(wrapper1.findAll('.is-checked').length).toBe(1);
-
-		// 默认选中第三个单选框
-		expect(wrapper1.find('.is-checked')).toStrictEqual(radioList[2]);
-
-		// 手动选中第一个单选框
-		await wrapper1.find('input[type="radio"][value="1"]').setValue();
-		expect(wrapper1.findAll('.is-checked').length).toBe(1);
-		expect(wrapper1.find('.is-checked')).toStrictEqual(radioList[0]);
-
-		// change 事件应该正常触发
-		expect(wrapper1.emitted()).toHaveProperty('change');
-	});
-
 	test('V3Radio 组件应该正常接收【border】属性，并展示为带有边框的单选按钮', async () => {
-		const wrapper1 = mount(V3Radio);
+		const wrapper = mount(V3Radio);
 
 		// 默认状态下，不带有边框
-		expect(wrapper1.find('.is-bordered').exists()).toBeFalsy();
+		expect(wrapper.find('.is-bordered').exists()).toBeFalsy();
 
 		// 手动设置 border
-		await wrapper1.setProps({
+		await wrapper.setProps({
 			border: true,
 		});
-		expect(wrapper1.find('.is-bordered').exists()).toBeTruthy();
+		expect(wrapper.find('.is-bordered').exists()).toBeTruthy();
 	});
 
 	test('V3Radio 组件应该正常接收【disabled】属性，并禁用特定的单选按钮', async () => {
-		const wrapper1 = mount(V3Radio);
+		const wrapper = mount(V3Radio);
 
 		// 默认状态下，都是可用的
-		expect(wrapper1.findAll('.is-disabled').length).toBe(0);
+		expect(wrapper.findAll('.is-disabled').length).toBe(0);
 
 		// 手动禁用单选按钮
-		await wrapper1.setProps({
+		await wrapper.setProps({
 			disabled: true,
 		});
-		expect(wrapper1.find('.is-disabled').exists()).toBeTruthy();
+		expect(wrapper.find('.is-disabled').exists()).toBeTruthy();
 	});
 
 	test('V3Radio 组件可以接收【size】配置项，用来控制单选框的尺寸', async () => {
