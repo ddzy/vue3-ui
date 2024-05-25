df4a9c56c783cffb4b932c57bb36d2449dbcc889
ddzy <1766083035@qq.com>
2024-05-25 15:14:53 +0800

    test(RadioGroup): RadioGroup 组件单元测试

--- jest.config.ts
+++ jest.config.ts
@@ -23,7 +23,7 @@ export default {
 		],
 	},
 	// testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
-	testRegex: '/__tests__/RadioButton\\.test\\.ts$',
+	testRegex: '/__tests__/RadioGroup\\.test\\.ts$',
 	testPathIgnorePatterns: ['Select\\.test\\.ts'],
 	moduleFileExtensions: ['vue', 'js', 'ts'],
 	moduleNameMapper: {
