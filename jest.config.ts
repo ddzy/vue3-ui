2131b1af195ea7010594b86db8ff2366ea46e705
ddzy <1766083035@qq.com>
2024-05-25 11:34:29 +0800

    test(InputNumber): InputNumber 组件单元测试

--- jest.config.ts
+++ jest.config.ts
@@ -23,7 +23,7 @@ export default {
 		],
 	},
 	// testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
-	testRegex: '/__tests__/Input\\.test\\.ts$',
+	testRegex: '/__tests__/InputNumber\\.test\\.ts$',
 	testPathIgnorePatterns: ['Select\\.test\\.ts'],
 	moduleFileExtensions: ['vue', 'js', 'ts'],
 	moduleNameMapper: {
