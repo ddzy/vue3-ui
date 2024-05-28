c784b4bceac4ad9705b18181839b8478e82ff16e
ddzy <1766083035@qq.com>
2024-05-28 15:03:58 +0800

    feat(global.d.ts): 全局组件声明文件新增 Icon

--- public/typings/components/icon.d.ts
+++ public/typings/components/icon.d.ts
@@ -2,6 +2,9 @@ import {
 	IIconAllProps as IIconParkProps,
 	IconType as IIconParkType,
 } from '@icon-park/vue-next/es/all';
+import { SFCWithInstall } from '../index';
 
 export interface IIconProps extends IIconParkProps {}
 export type IIconType = IIconParkType | string;
+
+export const V3Icon: SFCWithInstall<IIconProps>;
