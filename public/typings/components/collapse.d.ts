174d420fee2e43878826b19f615e0d6029e927bb
ddzy <1766083035@qq.com>
2024-07-06 21:12:36 +0800

    feat(Collapse, CollapseItem): Collapse组件类型声明

---
+++ public/typings/components/collapse.d.ts
@@ -0,0 +1,19 @@
+export interface ICollapseProps {
+	modelValue?: ICollapseModelValue;
+	accordion?: boolean;
+	plain?: boolean;
+	displayStrategy?: ICollapseDisplayStrategy;
+	arrowPlacement?: ICollapseArrowPlacement;
+	triggerArea?: ICollapseTriggerArea;
+}
+export type ICollapseModelValue = string | string[] | number | number[];
+export type ICollapseDisplayStrategy = 'if' | 'show';
+export type ICollapseArrowPlacement = 'start' | 'end';
+export type ICollapseTriggerArea = Array<'icon' | 'title' | 'extra'>;
+
+export interface ICollapseItemProps {
+	name?: string | number;
+	title?: string;
+	disabled?: boolean;
+	displayStrategy?: ICollapseDisplayStrategy;
+}
