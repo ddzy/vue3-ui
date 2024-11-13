d97ef2b8087ecc2cae6387d2062a8b989df1bb14
ddzy <1766083035@qq.com>
2024-11-13 16:53:55 +0800

    feat(Pagination): 定义分页组件可配置参数

--- packages/components/pagination/lib/Pagination.vue
+++ packages/components/pagination/lib/Pagination.vue
@@ -2,8 +2,40 @@
 	<div class="v3-pagination">分页</div>
 </template>
 <script lang="ts" setup>
+import { IPaginationProps } from '@/public/typings';
+
 defineOptions({
 	name: 'V3Pagination',
+});
+
+const props = withDefaults(defineProps<IPaginationProps>(), {
+	page: 1,
+	pageSize: 20,
+	total: 0,
+	pageSizes: () => [
+		{
+			label: `10条/页`,
+			value: 10,
+		},
+		{
+			label: `20条/页`,
+			value: 20,
+		},
+		{
+			label: `50条/页`,
+			value: 50,
+		},
+		{
+			label: `100条/页`,
+			value: 100,
+		},
+	],
+	showPageSizePicker: true,
+	showPageJumper: true,
+	displayOrder: () => ['pages', 'page-size-picker', 'page-jumper'],
+	displayPageCount: 9,
+	onPageChange: undefined,
+	onPageSizeChange: undefined,
 });
 </script>
 <style lang="scss">
--- public/typings/components/pagination.d.ts
+++ public/typings/components/pagination.d.ts
@@ -1,6 +1,17 @@
 import { SFCWithInstall } from "../utils/util";
 
-export interface IPaginationProps { }
+export interface IPaginationProps {
+  page?: number;
+  pageSize?: number;
+  total?: number;
+  pageSizes?: Array<number | { label: string, value: number }>;
+  showPageSizePicker?: boolean;
+  showPageJumper?: boolean;
+  displayOrder?: Array<'pages' | 'page-size-picker' | 'page-jumper'>;
+  displayPageCount?: number;
+  onPageChange?: (options: { page: number, pageSize: number; total: number }) => void;
+  onPageSizeChange?: (options: { page: number; pageSize: number; total: number }) => void;
+}
 
 
 export const V3Pagination: SFCWithInstall<IPaginationProps>;
